e5b0afc1ac4a38e88d24aabbed856a708e5ec68d
Liu <rsl22@ic.ac.uk>
2024-11-11 17:56:28 +0000

    fixed digital filter code, created main BioWave page

---
+++ BioWave.m
@@ -0,0 +1,13 @@
+close all;
+clear all;
+clc;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%% Main page for calling all the individual functions %%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+load('JS00001', 'val'); % load in the ECG data from saved file
+
+filtered_ecg = ECG_digital_filter(val); % calling the function that returns the filtered ECG data as an output
+
+
